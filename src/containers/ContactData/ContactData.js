2ce2ca6b1d683f71956a1d0cf3ffa4cf2561d92b
Abby Low <abby.low@plus65.com.sg>
2020-09-23 18:21:20 +0800

    display form field dynamically

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -4,14 +4,62 @@ import axios from '../../axios-orders';
 import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
+import Input from '../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Faster' },
+                        { value: 'cheapest', displayValue: 'Cheapest' },
+                    ],
+                    placeholder: 'Delivery Method'
+                },
+                value: ''
+            },
         },
         loading: false
     }
@@ -23,16 +71,7 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Max',
-                address: {
-                    street: 'test street 1',
-                    zipCode: '23343',
-                    country: 'SG'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+
         }
         // firebase endpoint needs to append .json to make it work (json like nested structure)
         axios.post('/orders.json', order)
@@ -46,15 +85,30 @@ class ContactData extends Component {
     }
 
     render() {
-        let form = (<form>
-            <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-            <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-            <input className={classes.Input} type="text" name="street" placeholder="Street" />
-            <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-            <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-        </form>);
-        if(this.state.loading) {
-            form = <Spinner/>;
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+
+        let form = (
+            <form>
+                {
+                    formElementsArray.map(formElement => (
+                        <Input
+                            key={formElement.id}
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value}
+                        />
+                    ))
+                }
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>);
+        if (this.state.loading) {
+            form = <Spinner />;
         }
         return (
             <div className={classes.ContactData}>
