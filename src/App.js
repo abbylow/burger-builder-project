d6bfe5c2165d6d12f095ed12183896283b7810bc
Abby Low <abby.low@plus65.com.sg>
2020-09-28 17:47:46 +0800

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import Checkout from './containers/Checkout/Checkout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
-import Logout from './containers/Auth/Logout/Logout';
+// import Orders from './containers/Orders/Orders';
+// import Auth from './containers/Auth/Auth';
+// import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+
+const Auth = React.lazy(() => import('./containers/Auth/Auth'));  //only default export is supported
+const Orders = React.lazy(() => import('./containers/Orders/Orders'));  
+const Logout = React.lazy(() => import('./containers/Auth/Logout/Logout'));  
 
 class App extends Component {
   componentDidMount() {
     this.props.onTryAutoLogin();
   }
+
   render() {
     let routes = (
       <Switch>
@@ -39,7 +44,9 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          {routes}
+          <Suspense fallback={<div>Loading...</div>}>
+            {routes}
+          </Suspense>
         </Layout>
       </div>
     );
