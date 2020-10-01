cf4feea246b62991e168f87fc2a8ab381697b301
Abby Low <abby.low@plus65.com.sg>
2020-10-02 02:12:30 +0800

    custom hook

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,38 +1,18 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
+import useHttpErrorHandler from '../../hooks/http-error-handler';
 
 const withErrorHandler = (WrapperComponent, axios) => {
     // anonymous class because never call the name
     return props => {
-        const [error, setError] = useState(null);
-
-        // componentWillMount: just put in this place instead of inside a useEffect hook
-        const reqInterceptor = axios.interceptors.request.use(req => {
-            setError(null); //clear the error when sending request
-            return req;
-        });
-
-        const resInterceptor = axios.interceptors.response.use(res => res, err => {
-            setError(err);
-        });
-
-        useEffect(() => {
-            return () => {
-                axios.interceptors.request.eject(reqInterceptor);
-                axios.interceptors.response.eject(resInterceptor);
-            }
-        }, [reqInterceptor, resInterceptor]);
-
-        const errorConfirmedHandler = () => {
-            setError(null);
-        }
+       const [error, clearError] = useHttpErrorHandler(axios);
 
         return (
             <Fragment>
                 <Modal
                     show={error}
-                    modalClosed={errorConfirmedHandler}
+                    modalClosed={clearError}
                 >
                     {error ? error.message : null}
                 </Modal>
